1cf772aa45fedb0ac208525872a160aa3b37f771
Mark Pruitt <mark.pruitt@smokymountainsoftworks.com>
2021-06-23 17:17:12 -0400

    Initial Commit

---
+++ Humantiv2/Humantiv2App.swift
@@ -0,0 +1,17 @@
+//
+//  Humantiv2App.swift
+//  Humantiv2
+//
+//  Created by Mark Pruit on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Humantiv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
